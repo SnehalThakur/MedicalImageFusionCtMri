d1e72a2674b88774105aef8f7182b456a3e9d943
Snehal Thakur <snehal.hadoop@gmail.com>
2023-04-24 21:27:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import os
 from datetime import datetime
 from flask import Flask, render_template, request, url_for
-import numpy as np 
+import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm 
+import matplotlib.cm as cm
 import cv2
 import imageio
 import scipy.ndimage as ndi
@@ -23,17 +23,18 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Our methods
 def convertToIntList(arr):
-    result=[]
+    result = []
     for q in arr.strip(']][[').split('],['):
-        x=[]
+        x = []
         for i in q.split(','):
-            x.append(int(i,10))
+            x.append(int(i, 10))
         result.append(x)
     return result
 
+
 def procrustes(X, Y, scaling=True, reflection='best'):
-    n,m = X.shape
-    ny,my = Y.shape
+    n, m = X.shape
+    ny, my = Y.shape
 
     muX = X.mean(0)
     muY = Y.mean(0)
@@ -41,8 +42,8 @@ def procrustes(X, Y, scaling=True, reflection='best'):
     X0 = X - muX
     Y0 = Y - muY
 
-    ssX = (X0**2.).sum()
-    ssY = (Y0**2.).sum()
+    ssX = (X0 ** 2.).sum()
+    ssY = (Y0 ** 2.).sum()
 
     # centred Frobenius norm
     normX = np.sqrt(ssX)
@@ -53,11 +54,11 @@ def procrustes(X, Y, scaling=True, reflection='best'):
     Y0 /= normY
 
     if my < m:
-        Y0 = np.concatenate((Y0, np.zeros(n, m-my)),0)
+        Y0 = np.concatenate((Y0, np.zeros(n, m - my)), 0)
 
     # optimum rotation matrix of Y
     A = np.dot(X0.T, Y0)
-    U,s,Vt = np.linalg.svd(A,full_matrices=False)
+    U, s, Vt = np.linalg.svd(A, full_matrices=False)
     V = Vt.T
     T = np.dot(V, U.T)
 
@@ -68,7 +69,7 @@ def procrustes(X, Y, scaling=True, reflection='best'):
 
         # if that's not what was specified, force another reflection
         if reflection != have_reflection:
-            V[:,-1] *= -1
+            V[:, -1] *= -1
             s[-1] *= -1
             T = np.dot(V, U.T)
 
@@ -80,26 +81,27 @@ def procrustes(X, Y, scaling=True, reflection='best'):
         b = traceTA * normX / normY
 
         # standarised distance between X and b*Y*T + c
-        d = 1 - traceTA**2
+        d = 1 - traceTA ** 2
         # transformed coords
-        Z = normX*traceTA*np.dot(Y0, T) + muX
+        Z = normX * traceTA * np.dot(Y0, T) + muX
 
     else:
         b = 1
-        d = 1 + ssY/ssX - 2 * traceTA * normY / normX
-        Z = normY*np.dot(Y0, T) + muX
+        d = 1 + ssY / ssX - 2 * traceTA * normY / normX
+        Z = normY * np.dot(Y0, T) + muX
 
     # transformation matrix
     if my < m:
-        T = T[:my,:]
-    c = muX - b*np.dot(muY, T)
-    #rot =1
-    #scale=2
-    #translate=3
-    #transformation values 
-    tform = {'rotation':T, 'scale':b, 'translation':c}
+        T = T[:my, :]
+    c = muX - b * np.dot(muY, T)
+    # rot =1
+    # scale=2
+    # translate=3
+    # transformation values
+    tform = {'rotation': T, 'scale': b, 'translation': c}
 
     return d, Z, tform
+
 
 # VGG19 CNN For Fusion
 class VGG19(torch.nn.Module):
@@ -118,6 +120,7 @@ class VGG19(torch.nn.Module):
             if idx == 3:
                 feature_maps.append(x)
         return feature_maps
+
 
 class Fusion:
     def __init__(self, input):
@@ -178,12 +181,12 @@ class Fusion:
             for sum_maps in zip(*imgs_sum_maps):
                 features = torch.cat(sum_maps, dim=1)
                 weights = self._softmax(F.interpolate(features,
-                                        size=self.images_to_tensors[0].shape[2:]))
+                                                      size=self.images_to_tensors[0].shape[2:]))
                 weights = F.interpolate(weights,
                                         size=self.images_to_tensors[0].shape[2:])
                 current_fusion = torch.zeros(self.images_to_tensors[0].shape)
                 for idx, tensor_img in enumerate(self.images_to_tensors):
-                    current_fusion += tensor_img * weights[:,idx]
+                    current_fusion += tensor_img * weights[:, idx]
                 if max_fusion is None:
                     max_fusion = current_fusion
                 else:
@@ -193,63 +196,65 @@ class Fusion:
             if output.ndim == 3:
                 output = np.transpose(output, (1, 2, 0))
             return output
-        
-        
+
     def _RGB_to_YCbCr(self, img_RGB):
-            """
+        """
             A private method which converts an RGB image to YCrCb format
             """
-            img_RGB = img_RGB.astype(np.float32) / 255.
-            return cv2.cvtColor(img_RGB, cv2.COLOR_RGB2YCrCb)
+        img_RGB = img_RGB.astype(np.float32) / 255.
+        return cv2.cvtColor(img_RGB, cv2.COLOR_RGB2YCrCb)
 
     def _YCbCr_to_RGB(self, img_YCbCr):
-            """
+        """
             A private method which converts a YCrCb image to RGB format
             """
-            img_YCbCr = img_YCbCr.astype(np.float32)
-            return cv2.cvtColor(img_YCbCr, cv2.COLOR_YCrCb2RGB)
+        img_YCbCr = img_YCbCr.astype(np.float32)
+        return cv2.cvtColor(img_YCbCr, cv2.COLOR_YCrCb2RGB)
 
     def _is_gray(self, img):
-            """
+        """
             A private method which returns True if image is gray, otherwise False
             """
-            if len(img.shape) < 3:
-                return True
-            if img.shape[2] == 1:
-                return True
-            b, g, r = img[:,:,0], img[:,:,1], img[:,:,2]
-            if (b == g).all() and (b == r).all():
-                return True
-            return False
+        if len(img.shape) < 3:
+            return True
+        if img.shape[2] == 1:
+            return True
+        b, g, r = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+        if (b == g).all() and (b == r).all():
+            return True
+        return False
 
     def _softmax(self, tensor):
-            """
+        """
             A private method which compute softmax ouput of a given tensor
             """
-            tensor = torch.exp(tensor)
-            tensor = tensor / tensor.sum(dim=1, keepdim=True)
-            return tensor
+        tensor = torch.exp(tensor)
+        tensor = tensor / tensor.sum(dim=1, keepdim=True)
+        return tensor
 
     def _tranfer_to_tensor(self):
-            """
+        """
             A private method to transfer all input images to PyTorch tensors
             """
-            self.images_to_tensors = []
-            for image in self.normalized_images:
-                np_input = image.astype(np.float32)
-                if np_input.ndim == 2:
-                    np_input = np.repeat(np_input[None, None], 3, axis=1)
-                else:
-                    np_input = np.transpose(np_input, (2, 0, 1))[None]
-                if self.device == "cuda":
-                    self.images_to_tensors.append(torch.from_numpy(np_input).cuda())
-                else:
-                    self.images_to_tensors.append(torch.from_numpy(np_input)) 
+        self.images_to_tensors = []
+        for image in self.normalized_images:
+            np_input = image.astype(np.float32)
+            if np_input.ndim == 2:
+                np_input = np.repeat(np_input[None, None], 3, axis=1)
+            else:
+                np_input = np.transpose(np_input, (2, 0, 1))[None]
+            if self.device == "cuda":
+                self.images_to_tensors.append(torch.from_numpy(np_input).cuda())
+            else:
+                self.images_to_tensors.append(torch.from_numpy(np_input))
 
-# Routes
+            # Routes
+
+
 @app.route("/")
 def index():
     return render_template("form.html")
+
 
 @app.route("/upload", methods=['POST'])
 def upload():
@@ -257,24 +262,25 @@ def upload():
     if not os.path.isdir(target):
         os.mkdir(target)
 
-    mri_file=request.files['mri']
-    ct_file=request.files['ct']
+    mri_file = request.files['mri']
+    ct_file = request.files['ct']
 
     destination1 = "/".join([target, "mri.jpg"])
     mri_file.save(destination1)
 
     destination2 = "/".join([target, "ct.jpg"])
     ct_file.save(destination2)
-    
-    points = request.form["points"] #no of points
+
+    points = request.form["points"]  # no of points
 
     return render_template("registration.html", points=points)
 
-@app.route("/register",methods=['POST'])
+
+@app.route("/register", methods=['POST'])
 def register():
     global mriCoord, ctCoord
-    mriCoord=convertToIntList(request.form['mriCoord'])
-    ctCoord=convertToIntList(request.form['ctCoord'])
+    mriCoord = convertToIntList(request.form['mriCoord'])
+    ctCoord = convertToIntList(request.form['ctCoord'])
 
     # Registration notebook code
     ct = cv2.imread('static/ct.jpg', 0)
@@ -282,25 +288,27 @@ def register():
     X_pts = np.asarray(ctCoord)
     Y_pts = np.asarray(mriCoord)
 
-    d,Z_pts,Tform = procrustes(X_pts,Y_pts)
+    d, Z_pts, Tform = procrustes(X_pts, Y_pts)
     R = np.eye(3)
-    R[0:2,0:2] = Tform['rotation']
+    R[0:2, 0:2] = Tform['rotation']
 
-    S = np.eye(3) * Tform['scale'] 
-    S[2,2] = 1
+    S = np.eye(3) * Tform['scale']
+    S[2, 2] = 1
     t = np.eye(3)
-    t[0:2,2] = Tform['translation']
-    M = np.dot(np.dot(R,S),t.T).T
-    h=ct.shape[0]
-    w=ct.shape[1]
-    tr_Y_img = cv2.warpAffine(mri,M[0:2,:],(h,w))
+    t[0:2, 2] = Tform['translation']
+    M = np.dot(np.dot(R, S), t.T).T
+    h = ct.shape[0]
+    w = ct.shape[1]
+    tr_Y_img = cv2.warpAffine(mri, M[0:2, :], (h, w))
     cv2.imwrite("static/mri_registered.jpg", tr_Y_img)
 
     return "something"
 
+
 @app.route("/registerimage")
 def registerimage():
     return render_template("imageregistration.html")
+
 
 @app.route("/fusion")
 def fusion():
@@ -312,8 +320,8 @@ def fusion():
     coeffs2 = pywt.dwt2(mri_image, 'haar')
     LL, (LH, HL, HH) = coeffs2
     for i, a in enumerate([LL, LH, HL, HH]):
-        path='static/mri_'+str(i)+'.jpg'
-        cv2.imwrite(path,a)
+        path = 'static/mri_' + str(i) + '.jpg'
+        cv2.imwrite(path, a)
 
     # Load CT Image
     ct_image = cv2.imread('static/ct.jpg')
@@ -324,10 +332,10 @@ def fusion():
     LL, (LH, HL, HH) = coeffs2
 
     for i, a in enumerate([LL, LH, HL, HH]):
-        path='static/ct_'+str(i)+'.jpg'
-        cv2.imwrite(path,a)
+        path = 'static/ct_' + str(i) + '.jpg'
+        cv2.imwrite(path, a)
 
-    input_images=[]
+    input_images = []
     mri = cv2.imread('static/mri_0.jpg')
     mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
 
@@ -339,7 +347,7 @@ def fusion():
     fusion_img = FU.fuse()
     cv2.imwrite('static/fusion_0.jpg', fusion_img)
 
-    input_images=[]
+    input_images = []
     mri = cv2.imread('static/mri_1.jpg')
     mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
 
@@ -351,7 +359,7 @@ def fusion():
     fusion_img = FU.fuse()
     cv2.imwrite('static/fusion_1.jpg', fusion_img)
 
-    input_images=[]
+    input_images = []
     mri = cv2.imread('static/mri_2.jpg')
     mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
 
@@ -363,7 +371,7 @@ def fusion():
     fusion_img = FU.fuse()
     cv2.imwrite('static/fusion_2.jpg', fusion_img)
 
-    input_images=[]
+    input_images = []
     mri = cv2.imread('static/mri_3.jpg')
     mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
 
@@ -374,7 +382,7 @@ def fusion():
     FU = Fusion(input_images)
     fusion_img = FU.fuse()
     cv2.imwrite('static/fusion_3.jpg', fusion_img)
-    
+
     fusion_0 = cv2.imread('static/fusion_0.jpg')
     fusion_0 = cv2.cvtColor(fusion_0, cv2.COLOR_BGR2GRAY)
 
@@ -387,58 +395,59 @@ def fusion():
     fusion_3 = cv2.imread('static/fusion_3.jpg')
     fusion_3 = cv2.cvtColor(fusion_3, cv2.COLOR_BGR2GRAY)
 
-    coeffs=(fusion_0,(fusion_1,fusion_2,fusion_3))
-    fusion=pywt.idwt2(coeffs,'haar')
-    cv2.imwrite('static/fusion.jpg',fusion)
+    coeffs = (fusion_0, (fusion_1, fusion_2, fusion_3))
+    fusion = pywt.idwt2(coeffs, 'haar')
+    cv2.imwrite('static/fusion.jpg', fusion)
 
     return render_template("fusion.html")
+
 
 @app.route("/segmentation")
 def segmentation():
     img = cv2.imread("static/fusion.jpg")
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
 
     ret, markers = cv2.connectedComponents(thresh)
 
-    marker_area = [np.sum(markers==m) for m in range(np.max(markers)) if m!=0] 
-    largest_component = np.argmax(marker_area)+1 #Add 1 since we dropped zero above                        
-    brain_mask = markers==largest_component
+    marker_area = [np.sum(markers == m) for m in range(np.max(markers)) if m != 0]
+    largest_component = np.argmax(marker_area) + 1  # Add 1 since we dropped zero above
+    brain_mask = markers == largest_component
 
     brain_out = img.copy()
-    brain_out[brain_mask==False] = (0,0,0)
+    brain_out[brain_mask == False] = (0, 0, 0)
 
     img = cv2.imread("static/fusion.jpg")
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     # noise removal
-    kernel = np.ones((3,3),np.uint8)
-    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
 
     # sure background area
-    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
 
     # Finding sure foreground area
-    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
-    ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
 
     # Finding unknown region
     sure_fg = np.uint8(sure_fg)
-    unknown = cv2.subtract(sure_bg,sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
 
     # Marker labelling
     ret, markers = cv2.connectedComponents(sure_fg)
 
     # Add one to all labels so that sure background is not 0, but 1
-    markers = markers+1
+    markers = markers + 1
 
     # Now, mark the region of unknown with zero
-    markers[unknown==255] = 0
-    markers = cv2.watershed(img,markers)
-    img[markers == -1] = [255,0,0]
+    markers[unknown == 255] = 0
+    markers = cv2.watershed(img, markers)
+    img[markers == -1] = [255, 0, 0]
 
-    im1 = cv2.cvtColor(img,cv2.COLOR_HSV2RGB)
+    im1 = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
     cv2.imwrite("static/segmented.jpg", im1)
 
     return render_template("segmentation.html")
@@ -450,7 +459,6 @@ def add_header(response):
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Expires'] = '0'
     return response
-
 
 
 if __name__ == "__main__":
